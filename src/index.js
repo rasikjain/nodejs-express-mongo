0dfe57ededdf670d3d15bbbef464fd32646c49a4
“Rasik <jainrasik@gmail.com>
2019-03-05 17:30:13 +0530

    setting up express

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
 
-console.log('Hello Node.js project');
-console.log(process.env.MY_SECRET);
+var port = parseInt(process.env.PORT_NUMBER) || 4000;
+const app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log('App listening on port: ' + port);
+});
