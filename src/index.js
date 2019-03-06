f4023caaa7ac55eff90aabbe58407a9558545546
“Rasik <jainrasik@gmail.com>
2019-03-06 13:14:55 +0530

    setting up mongodb with initial seeding and node routing

--- src/index.js
+++ src/index.js
@@ -1,15 +1,85 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
+import models, { connectDb } from './models';
+import routes from './routes';
 
 var port = parseInt(process.env.PORT_NUMBER) || 4000;
 const app = express();
+const eraseDatabaseOnSync = true;
+
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.context = {
+    models,
+    me: models.users[1]
+  };
+  next();
+});
+
+app.use('/session', routes.session);
+app.use('/users', routes.user);
+app.use('/messages', routes.message);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  return res.send('Received a GET HTTP method');
 });
 
-app.listen(port, () => {
-  console.log('App listening on port: ' + port);
+app.post('/', (req, res) => {
+  return res.send('Received a POST HTTP method');
 });
+
+app.put('/', (req, res) => {
+  return res.send('Received a PUT HTTP method');
+});
+
+app.delete('/', (req, res) => {
+  return res.send('Received a DELETE HTTP method');
+});
+
+connectDb().then(async () => {
+  if (eraseDatabaseOnSync) {
+    await Promise.all([
+      models.User.deleteMany({}),
+      models.Message.deleteMany({})
+    ]);
+  }
+  createUsersWithMessages();
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+});
+
+const createUsersWithMessages = async () => {
+  const user1 = new models.User({
+    username: 'test@test.com'
+  });
+
+  const user2 = new models.User({
+    username: 'ddavids'
+  });
+
+  const message1 = new models.Message({
+    text: 'Published the Road to learn React',
+    user: user1.id
+  });
+
+  const message2 = new models.Message({
+    text: 'Happy to release ...',
+    user: user2.id
+  });
+
+  const message3 = new models.Message({
+    text: 'Published a complete ...',
+    user: user2.id
+  });
+
+  await message1.save();
+  await message2.save();
+  await message3.save();
+
+  await user1.save();
+  await user2.save();
+};
---
+++ src/routes/session.js
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+const router = Router();
+
+router.get('/', (req, res) => {
+  return res.send(req.context.models.users[req.context.me.id]);
+});
+
+export default router;
